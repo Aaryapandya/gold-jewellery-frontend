c18b70dab1290f580050b93e0aa5256e577d6a84
Aarya Pandya <aarya.pandya1@tesco.com>
2026-09-22 15:09:12 +0530

    url change

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -15,7 +15,7 @@ import axios, {
 import Cookies from "js-cookie";
 
 const BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8080/api/v1";
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://alluring-embrace-production.up.railway.app/api/v1";
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
